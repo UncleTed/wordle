9499de043475edacf7cbbc86bab85e0a24fc12d4
TLAY <tlayher@gmail.com>
2022-05-28 15:15:21 -0400

    teds cheats

--- teds_cheats.js
+++ teds_cheats.js
@@ -1,30 +1,31 @@
-
 function getWordsArray() {
-    let url = 'https://www.nytimes.com/games/wordle/main.9622bc55.js';
-    let giantString = '';
-    let startOfSolution;
-    let endOfSolution;
-    let wordsString;
-    let openBracket;
-    let closeBracket;
-    let wordsArrayString;
-    let words ;
+  let url = "https://www.nytimes.com/games/wordle/main.9622bc55.js";
+  let giantString = "";
+  let startOfSolution;
+  let endOfSolution;
+  let wordsString;
+  let openBracket;
+  let closeBracket;
+  let wordsArrayString;
+  let words;
 
-    fetch(url).then((r)=>r.text()).then((r)=> giantString = r);
-    startOfSolution = giantString.indexOf('var ko=');
-    endOfSolution = giantString.indexOf(',wo=');
-    wordsString = giantString.substring(startOfSolution, endOfSolution);
-    openBracket = wordsString.indexOf('[');
-    closeBracket = wordsString.indexOf(']') + 1;
-    wordsArrayString = wordsString.substring(openBracket, closeBracket);
-    words = JSON.parse(wordsArrayString);
-    return words;
+  fetch(url)
+    .then((r) => r.text())
+    .then((r) => (giantString = r));
+  startOfSolution = giantString.indexOf("var ko=");
+  endOfSolution = giantString.indexOf(",wo=");
+  wordsString = giantString.substring(startOfSolution, endOfSolution);
+  openBracket = wordsString.indexOf("[");
+  closeBracket = wordsString.indexOf("]") + 1;
+  wordsArrayString = wordsString.substring(openBracket, closeBracket);
+  words = JSON.parse(wordsArrayString);
+  return words;
 }
 
-var 
-
 function getGoodLetters() {
-    var gameState = JSON.parse(localStorage.getItem('nyt-wordle-state'));
-    var boardState = gameState.boardState;
-    var evaluations = gameState.evaluations;
+  var gameState = JSON.parse(localStorage.getItem("nyt-wordle-state"));
+  var boardState = gameState.boardState;
+  var evaluations = gameState.evaluations;
+  console.log(boardState);
+  console.log(evaluations);
 }
